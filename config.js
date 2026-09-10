d373b8e420a27e288d31b98175c095d823d6cdd4
zeroseveneightzero-beep <zeroseveneightzero@gmail.com>
2026-09-10 22:56:41 +0800

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.EPIC_LEASE_CONFIG = {
   supabaseUrl: 'https://hcrkktwvyxspcyexvzes.supabase.co',
-  // Put your Supabase PUBLISHABLE key here. Never put a secret/service_role key here.
-  publishableKey: 'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE'
+  publishableKey: 'sb_publishable_Ry653OG6a0gFKKdGth4Sxw_F3tOZQvN'
 };
